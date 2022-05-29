cc2af5c9a328ddf984e56b09343eae249c54bfd4
JuliaOse <josei051@st.ug.edu.gh>
2022-05-29 20:26:10 +0000

    committed

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
         string name;
         cout<<"What is your name? \n";
-        cin>> name;
-        cout<<"Welcome," + name;
+        getline(cin, name);
+        cout<<"Welcome, " + name;
 
     return 0;
 }
